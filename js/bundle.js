8219a3698650b95de2461c0edb71f06052af5776
Vladyslav Shevchenko <imvladshevchenko@gmail.com>
2021-05-10 01:21:21 +0300

    test10

--- js/bundle.js
+++ js/bundle.js
@@ -1,15 +1,25 @@
 var wheel = document.getElementById("wheel");
 var wheelBtn = document.getElementById("button");
 var closePopup = document.getElementById("closePopup");
+var firstWrapper = document.getElementById("firstWrapper");
+var secondWrapper = document.getElementById("secondWrapper");
+var buttonText = document.getElementById("buttonText");
+
 
 var counter = 0;
 
 function animation() {
+  console.log(innerWidth)
   if (counter < 1) {
+    if (innerWidth < 500) {
+      buttonText.innerHTML = 'OK'
+    }
     wheel.classList.toggle("step-1");
     wheelBtn.classList.toggle("animation-in-process");
     counter++;
   } else {
+    firstWrapper.style.display = "none"
+    secondWrapper.style.display = "block"
     wheel.classList.toggle("step-2");
     wheelBtn.classList.toggle("animation-in-process");
     closePopup.style.display = "none";
